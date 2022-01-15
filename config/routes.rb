e76b5bf4d5ee8bd59baac8a5391723f0754b98d9
Andre Ferrer <andrerferrer@hotmail.com>
2022-01-15 14:08:00 -0300

    finish lovecode

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # INDEX
+  root to: 'pets#index'
+  # NEW
+  get 'pets/new', to: 'pets#new', as: :new_pet
+  # SHOW
+  get 'pets/:id', to: 'pets#show', as: :pet
+  # CREATE
+  post 'pets', to: 'pets#create'
+
+  # DESTROY
+  delete '/pets/:id', to: 'pets#destroy'
 end
+
+# ONLY IF you use the verb GET
+# you have to create a view.html.erb
